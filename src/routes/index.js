8d361390134c31676b4cca610a466e99129ad7f5
dinhvaren <hqhuyzzzz@gmail.com>
2025-03-19 22:40:32 +0700

    update page admin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const { home } = require('../app/controller/HomeController');
 const homeRouter = require('./home');
+const adminRouter = require('./admin');
 function route(app) {
     app.use('/', homeRouter);
+    app.use('/admin', adminRouter);
 }
 
 module.exports = route;
